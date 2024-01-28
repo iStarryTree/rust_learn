c7944f25a330ee8bcca0da413dd6f20d7a6a3629
starry_tree <starry_tree@outlook.com>
2024-01-28 11:56:53 +0800

    trace

--- jikeshijian/axum_helloworld/src/main.rs
+++ jikeshijian/axum_helloworld/src/main.rs
@@ -1,8 +1,11 @@
 use axum::{response::Html, routing::get, Router};
 use tower_http::services::{ServeDir, ServeFile};
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
     let serve_dir =
         ServeDir::new("assert2").not_found_service(ServeFile::new("assert2/index.html"));
 
@@ -10,13 +13,15 @@ async fn main() {
         .route("/foo", get(handler))
         .nest_service("/assets", ServeDir::new("assets"))
         .nest_service("/assert2", serve_dir.clone())
-        .fallback_service(serve_dir);
+        .fallback_service(serve_dir)
+        .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
 
-    println!("listening on {}", listener.local_addr().unwrap());
+    // println!("listening on {}", listener.local_addr().unwrap());
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
 
     axum::serve(listener, app).await.unwrap();
 }
