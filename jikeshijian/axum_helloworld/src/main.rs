7922dd6f17e112c0c1a4594859dc127126ccdb34
starry_tree <starry_tree@outlook.com>
2024-01-28 13:12:32 +0800

    form post
    post

--- jikeshijian/axum_helloworld/src/main.rs
+++ jikeshijian/axum_helloworld/src/main.rs
@@ -1,7 +1,8 @@
 use axum::extract::Query;
 use axum::response::IntoResponse;
-use serde::Deserialize;
+use axum::Form;
 use axum::{response::Html, routing::get, Router};
+use serde::Deserialize;
 use tower_http::services::{ServeDir, ServeFile};
 use tower_http::trace::TraceLayer;
 
@@ -15,6 +16,7 @@ async fn main() {
     let app = Router::new()
         .route("/foo", get(handler))
         .route("/query", get(query))
+        .route("/form", get(show_form).post(accept_form))
         .nest_service("/assets", ServeDir::new("assets"))
         .nest_service("/assert2", serve_dir.clone())
         .fallback_service(serve_dir)
@@ -45,4 +47,43 @@ struct InputParams {
 async fn query(Query(params): Query<InputParams>) -> impl IntoResponse {
     tracing::debug!("params={:?}", params);
     Html("<h3>Test query</h3>")
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct Input {
+    name: String,
+    email: String,
+}
+
+async fn accept_form(Form(input): Form<Input>) -> Html<&'static str> {
+    tracing::debug!("form={:?}", input);
+
+    Html("<h3>Test form</h3>")
+}
+
+async fn show_form() -> Html<&'static str> {
+    Html(
+        r#"
+        <!DOCTYPE html>
+        <html>
+            <head></head>
+            <body>
+                <form action="/form" method="post">
+                    <label for="name">
+                        Enter your name:
+                        <input type="text" id="name" name="name">
+                    </label>
+
+                    <label for="email">
+                        Enter your email:
+                        <input type="text" id="email" name="email">
+                    </label>
+
+                    <input type="submit" value="Submit">
+                </form>
+            </body>
+        </html>
+    "#,
+    )
 }
