27c082cc830f10d3c31f3b3bf63f9857699888d0
starry_tree <starry_tree@outlook.com>
2024-04-14 22:03:26 +0800

    first

--- rust_course/03/minigrep/src/main.rs
+++ rust_course/03/minigrep/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+fn main(){
+    // let args:Vec<String> = env::args().collect();
+    let args = env::args().collect::<Vec<String>>();
+
+    let query= &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {} in {}", query, file_path);
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("should have been able to read the file");
+
+    print!("With text:\n{}", contents);
+
 }
