90d1e00fc058df937429e9a07a04ee63d592d18b
starry_tree <starry_tree@outlook.com>
2024-04-17 20:19:07 +0800

    线程等待

--- rust_course/04/thread_learn/src/main.rs
+++ rust_course/04/thread_learn/src/main.rs
@@ -1,7 +1,9 @@
 use std::thread;
 use std::time::Duration;
 
-fn main() {
+// 基本线程使用
+#[allow(dead_code)]
+fn thread_test1() {
     thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawnd thread!", i);
@@ -13,4 +15,27 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+}
+
+// 线程等待
+#[allow(dead_code)]
+fn thread_test2() {
+    let handle = thread::spawn(|| {
+        for i in 1..5 {
+            println!("hi number {} from the spawnd thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    handle.join().unwrap();
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
+
+fn main() {
+    // thread_test1();
+    thread_test2();
 }
