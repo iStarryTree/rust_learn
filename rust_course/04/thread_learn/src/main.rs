30f36236aad9bed1835a74b1ab8226e0d8f1de03
starry_tree <starry_tree@outlook.com>
2024-04-18 22:13:39 +0800

    线程同步:消息传递

--- rust_course/04/thread_learn/src/main.rs
+++ rust_course/04/thread_learn/src/main.rs
@@ -1,5 +1,5 @@
 use std::cell::RefCell;
-use std::sync::{Arc, Barrier};
+use std::sync::{mpsc, Arc, Barrier};
 use std::thread;
 use std::time::Duration;
 
@@ -112,9 +112,149 @@ fn thread_test6() {
     FOO.with(|f| assert_eq!(*f.borrow(), 2));
 }
 // 结构体中使用
-thread_local!{
+thread_local! {
     static FOO: RefCell<usize> = RefCell::new(0);
-    
+
+}
+
+// 消息通道
+#[allow(dead_code)]
+fn thread_test7() {
+    let (tx, rx) = mpsc::channel();
+
+    // 创建线程，并发送消息
+    thread::spawn(move || {
+        tx.send(1).unwrap();
+    });
+
+    println!("receive {}", rx.recv().unwrap());
+}
+
+// try_recv
+#[allow(dead_code)]
+fn thread_test8() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        tx.send(1).unwrap();
+    });
+
+    print!("receive {}", rx.try_recv().unwrap()); // 没有消息会error
+}
+
+// 所有权
+#[allow(dead_code)]
+fn thread_test9() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let s = String::from("我，飞走了");
+        tx.send(s).unwrap();
+        // println!("send {}", s); // 没有所有权了
+    });
+
+    let received = rx.recv().unwrap();
+    println!("receive {}", received);
+}
+
+// 使用for进行循环接收
+#[allow(dead_code)]
+fn thread_test10() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("got: {}", received);
+    }
+}
+
+// 使用多发送者
+#[allow(dead_code)]
+fn thread_test11() {
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone(); // 不影响性能
+    thread::spawn(move || {
+        tx.send(String::from("hi from raw tx")).unwrap();
+    });
+
+    thread::spawn(move || {
+        tx1.send(String::from("hi from tx1")).unwrap();
+    });
+
+    for received in rx {
+        println!("got: {}", received);
+    }
+}
+
+// 异步通道
+#[allow(dead_code)]
+fn thread_test12() {
+    let (tx, rx) = mpsc::channel();
+
+    let handle = thread::spawn(move || {
+        println!("发送之前");
+        tx.send(1).unwrap();
+        println!("发送之后");
+    });
+
+    println!("睡眠之前");
+    thread::sleep(Duration::from_secs(1));
+    println!("睡眠之后");
+
+    println!("receive {}", rx.recv().unwrap());
+    handle.join().unwrap();
+}
+
+// 同步通道
+#[allow(dead_code)]
+fn thread_test13() {
+    let (tx, rx) = mpsc::sync_channel(1);
+
+    let handle = thread::spawn(move || {
+        println!("发送之前");
+        tx.send(1).unwrap();
+        println!("发送之后");
+    });
+
+    println!("睡眠之前");
+    thread::sleep(Duration::from_secs(1));
+    println!("睡眠之后");
+
+    println!("receive {}", rx.recv().unwrap());
+    handle.join().unwrap();
+}
+
+// 传输多种类型的数据
+#[allow(dead_code)]
+enum Fruit {
+    Apple(u8),
+    Orange(String),
+}
+fn thread_test14() {
+    let (tx, rx) = mpsc::channel();
+
+    tx.send(Fruit::Apple(2)).unwrap();
+    tx.send(Fruit::Orange(String::from("sweet"))).unwrap();
+
+    for _ in 0..2 {
+        match rx.recv().unwrap() {
+            Fruit::Apple(a) => println!("{} apples", a),
+            Fruit::Orange(o) => println!("{} oranges", o),
+        }
+    }
 }
 
 fn main() {
@@ -123,5 +263,15 @@ fn main() {
     // thread_test3();
     // thread_test4();
     // thread_test5();
-    thread_test6();
+    // thread_test6();
+
+    // 消息通道
+    // thread_test7();
+    // thread_test8();
+    // thread_test9();
+    // thread_test10();
+    // thread_test11();
+    // thread_test12();
+    // thread_test13();
+    thread_test14();
 }
