ec8232f26462358ab66af831296d2e7e6eab837c
starry_tree <starry_tree@outlook.com>
2024-07-31 11:06:22 +0800

    实现静态文件服务

--- jksj_tg/l2122_axum/src/main.rs
+++ jksj_tg/l2122_axum/src/main.rs
@@ -1,8 +1,15 @@
-use axum::{response::Html, Router, routing::get};
+use axum::{response::Html, routing::get, Router};
+use tower_http::services::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let serve_dir = ServeDir::new("assets2")
+        .not_found_service(ServeFile::new("assets2/index.html"));
+
+    let app = Router::new().route("/foo", get(handler))
+    .nest_service("/assets", ServeDir::new("assets"))
+    .nest_service("/assets2", serve_dir.clone())
+    .fallback_service(serve_dir);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
