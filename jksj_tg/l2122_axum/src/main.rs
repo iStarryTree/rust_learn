ff7a0d1deb12dd5ff2f77b3c1aa57338be54966a
starry_tree <starry_tree@outlook.com>
2024-07-31 11:39:47 +0800

    加入日志记录

--- jksj_tg/l2122_axum/src/main.rs
+++ jksj_tg/l2122_axum/src/main.rs
@@ -1,21 +1,27 @@
 use axum::{response::Html, routing::get, Router};
 use tower_http::services::{ServeDir, ServeFile};
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
-    let serve_dir = ServeDir::new("assets2")
-        .not_found_service(ServeFile::new("assets2/index.html"));
+    // 初始化日志
+    tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/foo", get(handler))
-    .nest_service("/assets", ServeDir::new("assets"))
-    .nest_service("/assets2", serve_dir.clone())
-    .fallback_service(serve_dir);
+    let serve_dir =
+        ServeDir::new("assets2").not_found_service(ServeFile::new("assets2/index.html"));
+
+    let app = Router::new()
+        .route("/foo", get(handler))
+        .nest_service("/assets", ServeDir::new("assets"))
+        .nest_service("/assets2", serve_dir.clone())
+        .fallback_service(serve_dir)
+        .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
 
-    println!("listening on {}", listener.local_addr().unwrap());
+    tracing::debug!("listening on 127.0.0.1:3000");
     axum::serve(listener, app).await.unwrap();
 }
 
